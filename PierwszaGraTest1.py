915bfaeeab05b4f2abb97634e10d2c9aa1052078
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2021-06-21 12:44:43 +0200

    Najnowsze

--- PierwszaGraTest1.py
+++ PierwszaGraTest1.py
@@ -1,6 +1,7 @@
 # importowanie biblioteki
 import pygame
 import time
+import random
 #inicjalizacja, rozpocznij program
 pygame.init()
 
@@ -22,9 +23,16 @@ speedR = 0
 speedU = 0
 speedD = 0
 
-
+#Dodanie przeciwnika
+przeciwnikimg=pygame.image.load("assets/przciwnik.png")
+przeciwnikX=random.randint(0,736)
+przeciwnikY=20
+speedE=2
 def gracz(x, y): #x- pozycja pionowa y-pozycja pozioma
     screen.blit(graczimg, (x, y)) # Blit oznacza narysuj
+
+def przeciwnik(x,y):
+    screen.blit(przeciwnikimg, (x, y))
 
 running = True # Zmienna running true (czyli gra jest otwarta)
 while running:    #Kiedy running jest True To gra działa cały czas
@@ -36,15 +44,15 @@ while running:    #Kiedy running jest True To gra działa cały czas
             running = False                #To running zmienia się na False i kończy działanie programu
         if wydarzenie.type == pygame.KEYDOWN:
             if wydarzenie.key == pygame.K_LEFT:
-                speedL = -1
+                speedL = -2
             if wydarzenie.key == pygame.K_RIGHT:
-                speedR = 1
+                speedR = 2
             if wydarzenie.key == pygame.K_UP:
                 speedU = -1
             if wydarzenie.key == pygame.K_DOWN:
                 speedD = 1
         if wydarzenie.type == pygame.KEYUP:
-            if wydarzenie.key == pygame.K_LEFT :
+            if wydarzenie.key == pygame.K_LEFT:
                 speedL = 0
             if wydarzenie.key == pygame.K_RIGHT:
                 speedR = 0
@@ -52,6 +60,7 @@ while running:    #Kiedy running jest True To gra działa cały czas
                 speedU = 0
             if wydarzenie.key == pygame.K_DOWN:
                 speedD = 0
+
 
     print(graczX , graczY)
 
@@ -64,15 +73,26 @@ while running:    #Kiedy running jest True To gra działa cały czas
         graczX=0
     elif graczX>=772:
         graczX=772
-
     if graczY<=0:
         graczY=0
     elif graczY >=536:
         graczY=536
 
+ #Ogranicz obszar ruchu przecniwnika
+    if przeciwnikX <= 0:
+        speedE*=-1   #Jeśli dochodzi do prawej krawędzi zmiena speed na druga strone i przeciwnik wraca.obniza o 32 piksele w dół
+        przeciwnikY+= 32
+    elif przeciwnikX >= 736:
+        speedE*= -1
+        przeciwnikY+= 32
+
+    przeciwnikX+=speedE
+
+
+
 
     gracz(graczX, graczY)  # to musimy dać po screen.fill. Teraz najpierw rysuje ekran a potem gracza
-
+    przeciwnik(przeciwnikX, przeciwnikY)
     time.sleep(0.005)
     pygame.display.update() # Co kazda klatke odswieża nam wyświetlacz # Dla pętli while jest wcięcie
 
